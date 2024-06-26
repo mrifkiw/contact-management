030f611d998944f17c22b9815bbbf8ac3033a79f
hadaniel69 <hadaniel69@gmail.com>
2024-06-26 22:25:21 +0000

    chore: setup database in idx

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -8,7 +8,14 @@
     pkgs.php82
     pkgs.php82Packages.composer
     pkgs.nodejs_20
+    pkgs.mariadb
   ];
+
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
+  };
+
   # Sets environment variables in the workspace
   env = {};
   idx = {
