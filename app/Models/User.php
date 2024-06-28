8815b12567805bbe1eaede317ff08e8e20c03421
hadaniel69 <hadaniel69@gmail.com>
2024-06-28 12:44:25 +0000

    feat: add model and controller for auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,46 +2,25 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Illuminate\Database\Relations\HasMany;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
 {
-    use HasFactory, Notifiable;
+    protected $table = "users";
+    protected $primaryKey = "id";
+    protected $keyType = "int";
+    public $timestamps = true;
+    public $incrementing = true;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        "username",
+        "password",
+        "name",
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+    
+    public function contacts(): HasMany {
+        return $this->hasMany(Contacts::class, "user_id", "id");
     }
 }
