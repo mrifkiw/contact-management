920f668567c38fc66fde598877549f9f49e89e54
mrifkiw <61343742+mrifkiw@users.noreply.github.com>
2024-06-29 20:02:07 +0700

    feat: update contact api

--- tests/Feature/ContactTest.php
+++ tests/Feature/ContactTest.php
@@ -6,6 +6,7 @@ use App\Models\Contact;
 use App\Models\User;
 use Database\Seeders\ContactSeeder;
 use Database\Seeders\UserSeeder;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
 class ContactTest extends TestCase
@@ -119,6 +120,54 @@ class ContactTest extends TestCase
                     "message" => [
                         "contact not found"
                     ]
+                ]
+            ]);
+    }
+
+    public function testUpdateSuccess()
+    {
+        $this->seed([UserSeeder::class, ContactSeeder::class]);
+
+        $contact = Contact::query()->limit(1)->first();
+        echo (User::query()->limit(1)->first());
+
+        $this->put("/api/contacts/" . $contact->id, [
+            "first_name" => "test2",
+            "last_name" => "test2",
+            "email" => "test2@pzn.com",
+            "phone" => "111112",
+        ], [
+            "Authorization" => "test"
+        ])->assertStatus(200)
+            ->assertJson([
+                "data" => [
+                    "first_name" => "test2",
+                    "last_name" => "test2",
+                    "email" => "test2@pzn.com",
+                    "phone" => "111112",
+                ]
+            ]);
+    }
+    public function testUpdateValidationError()
+    {
+        $this->seed([UserSeeder::class, ContactSeeder::class]);
+
+        $contact = Contact::query()->limit(1)->first();
+        echo (User::query()->limit(1)->first());
+
+        $this->put("/api/contacts/" . $contact->id, [
+            "first_name" => "",
+            "last_name" => "test2",
+            "email" => "test2@pzn.com",
+            "phone" => "111112",
+        ], [
+            "Authorization" => "test"
+        ])->assertStatus(400)
+            ->assertJson([
+                "errors" => [
+                    "first_name" => [
+                        "The first name field is required."
+                    ],
                 ]
             ]);
     }
